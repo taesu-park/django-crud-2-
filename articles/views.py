f1676dbab22dcb77823f710c7e7b605a1c74bffc
edutak <edutak.ssafy@gmail.com>
2019-09-10 11:30:05 +0900

    09 | Set URL namespace

--- articles/views.py
+++ articles/views.py
@@ -23,7 +23,7 @@ def create(request):
     #     'article': article
     # }
     # return render(request, 'articles/create.html', context)
-    return redirect(f'/articles/{article.pk}/')
+    return redirect('articles:detail', article.pk)
 
 def detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
@@ -35,7 +35,7 @@ def detail(request, article_pk):
 def delete(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     article.delete()
-    return redirect('/articles/')
+    return redirect('articles:index')
 
 def edit(request, article_pk):
     article = Article.objects.get(pk=article_pk)
@@ -49,4 +49,4 @@ def update(request, article_pk):
     article.title = request.GET.get('title')
     article.content = request.GET.get('content')
     article.save()
-    return redirect(f'/articles/{article.pk}/')
+    return redirect('articles:detail', article.pk)
