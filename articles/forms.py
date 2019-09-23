0b788f6ea03c99848c1d29167ebb93e745e4627e
edutak <edutak.ssafy@gmail.com>
2019-09-23 10:40:57 +0900

    20 | Change comment form to modelform

--- articles/forms.py
+++ articles/forms.py
@@ -1,29 +1,39 @@
 from django import forms
-from .models import Article
+from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
-    # 위젯 설정 2.
-    title = forms.CharField(
-        max_length=140, 
-        label='제목',
-        help_text='140자 이내로 작성바랍니다.',
-        widget=forms.TextInput(
-            attrs={
-                'placeholder': '제목을 입력바랍니다.'
-            }
-        )
-    )
-    content = forms.CharField(
-        label='내용',
-        widget=forms.Textarea(
-            attrs={
-                'placeholder': '내용을 입력바랍니다.'
-            }
-        )
-    )
     class Meta:
         model = Article
         fields = '__all__'
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ('article', )
+
+# class ArticleForm(forms.ModelForm):
+#     # 위젯 설정 2.
+#     title = forms.CharField(
+#         max_length=140, 
+#         label='제목',
+#         help_text='140자 이내로 작성바랍니다.',
+#         widget=forms.TextInput(
+#             attrs={
+#                 'placeholder': '제목을 입력바랍니다.'
+#             }
+#         )
+#     )
+#     content = forms.CharField(
+#         label='내용',
+#         widget=forms.Textarea(
+#             attrs={
+#                 'placeholder': '내용을 입력바랍니다.'
+#             }
+#         )
+#     )
+#     class Meta:
+#         model = Article
+#         fields = '__all__'
         # fields = ('title', )
         # exclude = ('title', )
         # 위젯 설정 1.
--- articles/views.py
+++ articles/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST, require_GET
 
-from .forms import ArticleForm
+from .forms import ArticleForm, CommentForm
 from .models import Article, Comment
 
 # Create your views here.
@@ -45,9 +45,11 @@ def detail(request, article_pk):
     # article = Article.objects.get(pk=article_pk)
     article = get_object_or_404(Article, pk=article_pk)
     comments = article.comment_set.all()
+    comment_form = CommentForm()
     context = {
         'article': article,
         'comments': comments,
+        'comment_form': comment_form
     }
     return render(request, 'articles/detail.html', context)
 
@@ -84,12 +86,26 @@ def update(request, article_pk):
 @require_POST
 def comment_create(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
-    comment = Comment()
-    comment.content = request.POST.get('comment_content')
-    comment.article = article
-    # comment.article_id = article_pk
-    comment.save()
+    # 1. modelform에 사용자 입력값 넣고
+    comment_form = CommentForm(request.POST)
+    # 2. 검증하고,
+    if comment_form.is_valid():
+    # 3. 맞으면 저장!
+        # 3-1. 사용자 입력값으로 comment instance 생성 (저장은 X)
+        comment = comment_form.save(commit=False)
+        # 3-2. FK 넣고 저장
+        comment.article = article
+        comment.save()
+    # 4. return redirect
+    else:
+        messages.success(request, '댓글이 형식이 맞지 않습니다.')
     return redirect('articles:detail', article_pk)
+
+    # comment = Comment()
+    # comment.content = request.POST.get('content')
+    # comment.article = article
+    # comment.save()
+    # return redirect('articles:detail', article_pk)
 
 @require_POST
 def comment_delete(request, article_pk, comment_pk):
